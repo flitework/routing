198dcc57fce8dfc553765b99c21b989efcdcdb1e
HMRDevil <ivanmezinov@mail.ru>
2023-10-15 04:08:41 +0300

    add RouteCollection

---
+++ RouteCollection.php
@@ -0,0 +1,65 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Flitework\Routing;
+
+use Flitework\Routing\Contracts\CollectionInterface;
+use Flitework\Routing\Route;
+use Flitework\Routing\Exception\RouteNotFoundException;
+
+/**
+ * 
+ * 
+ * @author Ivan Mezinov <ivanmezinov@mail.ru>
+ */
+class RouteCollection implements CollectionInterface
+{
+    /**
+     * @var array<name: string, Route>
+     */
+    private array $routes;
+    
+    public function add(string $name, Route $route = null): static
+    {
+        if ($route) {
+            $this->routes[$name] = $route;
+        }
+        
+        return $this;
+    }
+
+    public function clear(): void
+    {
+        $this->routes = [];
+    }
+
+    public function count(): int
+    {
+        return \count($this->routes);
+    }
+
+    public function get(string $name): Route
+    {
+        if (!$this->has($name)) {
+            throw new RouteNotFoundException($name);
+        }
+        
+        return $this->routes[$name];
+    }
+
+    public function has(string $name): bool
+    {
+        return array_key_exists($name, $this->routes);
+    }
+
+    public function remove(string $name): void
+    {
+        unset($this->routes[$name]);
+    }
+
+    public function all(): array
+    {
+        return $this->routes;
+    }
+}
