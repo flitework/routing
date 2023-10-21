7aa010382b860ab050bf909b17d432bb44aa861a
HMRDevil <ivanmezinov@mail.ru>
2023-10-21 19:46:50 +0300

    add LoaderManagerInterface

---
+++ Contracts/LoaderManagerInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Flitework\Routing\Contracts;
+
+/**
+ *
+ * @author Ivan Mezinov <ivanmezinov@mail.ru>
+ */
+interface LoaderManagerInterface
+{
+    public function load(string $resourse);
+}
